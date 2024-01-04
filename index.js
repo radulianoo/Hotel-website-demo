58fe608c4d1c7c576f71142a8266c098ca2be05e
Octav Radulian <octav@raduliano.tech>
2024-01-04 18:17:00 +0200

    Created about us section and products section.

--- index.js
+++ index.js
@@ -28,3 +28,26 @@
     ...scrollRevealOption,
     delay: 500,
   });
+
+  //about container
+  ScrollReveal().reveal(".about__image img", {
+    ...scrollRevealOption,
+    origin: "left",
+  });
+
+  ScrollReveal().reveal(".about__content .section__subheader", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  ScrollReveal().reveal(".about__content .section__header", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+  ScrollReveal().reveal(".about__content .section__description", {
+    ...scrollRevealOption,
+    delay: 1500,
+  });
+  ScrollReveal().reveal(".about__btn", {
+    ...scrollRevealOption,
+    delay: 2000,
+  });
