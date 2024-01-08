08fd2cf0a051710cb803013e404f63ae224c64e0
Octav Radulian <octav@raduliano.tech>
2024-01-08 12:48:55 +0200

    project reorganization

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-  const menuBtn = document.getElementById("menu-btn");
-  const navLinks = document.getElementById("nav-links");
-  const menuBtnIcon = menuBtn.querySelector("i");
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
 
-  menuBtn.addEventListener("click", ()=> {
-    navLinks.classList.toggle("open");
-    const isOpen = navLinks.classList.contains("open");
-    menuBtnIcon.setAttribute("class", isOpen? "ri-close-line" : "ri-menu-line");
-  });
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
 
-  navLinks.addEventListener("click", () => {
-    navLinks.classList.remove("open");
-    menuBtnIcon.setAttribute("class", "ri-menu-line");
-  });
+navLinks.addEventListener("click", () => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
 
-  const scrollRevealOption = {
-    distance: "50px",
-    origin: "bottom",
-    duration: 1000,
-  }
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
 
-  //header container
-  ScrollReveal().reveal(".header__container p", {
-    ...scrollRevealOption,
-  });
+//header container
+ScrollReveal().reveal(".header__container p", {
+  ...scrollRevealOption,
+});
 
-  ScrollReveal().reveal(".header__container h1", {
-    ...scrollRevealOption,
-    delay: 500,
-  });
+ScrollReveal().reveal(".header__container h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
 
-  //about container
-  ScrollReveal().reveal(".about__image img", {
-    ...scrollRevealOption,
-    origin: "left",
-  });
+//about container
+ScrollReveal().reveal(".about__image img", {
+  ...scrollRevealOption,
+  origin: "left",
+});
 
-  ScrollReveal().reveal(".about__content .section__subheader", {
-    ...scrollRevealOption,
-    delay: 500,
-  });
-  ScrollReveal().reveal(".about__content .section__header", {
-    ...scrollRevealOption,
-    delay: 1000,
-  });
-  ScrollReveal().reveal(".about__content .section__description", {
-    ...scrollRevealOption,
-    delay: 1500,
-  });
-  ScrollReveal().reveal(".about__btn", {
-    ...scrollRevealOption,
-    delay: 2000,
-  });
+ScrollReveal().reveal(".about__content .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".about__content .section__header", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".about__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".about__btn", {
+  ...scrollRevealOption,
+  delay: 1900,
+});
